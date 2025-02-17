abe152808b68f0d77efb1fe63501b8aeb3b3d224
kairatabdillaev <kajratabdillaev@gmail.com>
2025-02-17 21:48:15 +0600

    never give up

--- app/src/main/java/com/example/calculate/MainActivity.java
+++ app/src/main/java/com/example/calculate/MainActivity.java
@@ -12,12 +12,7 @@ import com.google.android.material.button.MaterialButton;
 public class MainActivity extends AppCompatActivity {
     private TextView result;
     private boolean isOperationClick;
-    private boolean isPlusOperation = false;
-    private boolean isMinusOperation = false;
-    private boolean isMultiplyOperation = false;
-    private boolean isDivideOperation = false;
-    private double first, second;
-    private Button btn_equal;
+    private int first, second;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,99 +20,54 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         result = findViewById(R.id.result);
-        btn_equal = findViewById(R.id.btn_equal);
     }
 
     public void oneClick(View view) {
-        btn_equal.setEnabled(true);
         String textButton = ((MaterialButton) view).getText().toString();
-
         if (textButton.equals("AC")) {
             result.setText("0");
-            first = 0;
-            second = 0;
-            isPlusOperation = false;
-            isMinusOperation = false;
-            isMultiplyOperation = false;
-            isDivideOperation = false;
         } else if (result.getText().toString().equals("0") || isOperationClick) {
             result.setText(textButton);
         } else {
             result.append(textButton);
         }
-
         isOperationClick = false;
     }
 
     public void oneOperationClick(View view) {
-        if (view.getId() == R.id.btn_plus) {
-            first = Double.parseDouble(result.getText().toString());
-            isPlusOperation = true;
-            isMinusOperation = false;
-            isMultiplyOperation = false;
-            isDivideOperation = false;
-        } else if (view.getId() == R.id.btn_minus) {
-            first = Double.parseDouble(result.getText().toString());
-            isMinusOperation = true;
-            isPlusOperation = false;
-            isMultiplyOperation = false;
-            isDivideOperation = false;
+        if (view.getId() == R.id.btn_minus) {
+            //нажимаем на минус
+            first = Integer.parseInt(result.getText().toString());
+
+        } else if (view.getId() == R.id.btn_plus) {
+            //нажимаем на плюс
+            first = Integer.parseInt(result.getText().toString());
+        }else if (view.getId() == R.id.btn_divide) {
+            //нажимаем на деление
+            first = Integer.parseInt(result.getText().toString());
         } else if (view.getId() == R.id.btn_multiply) {
-            first = Double.parseDouble(result.getText().toString());
-            isMultiplyOperation = true;
-            isPlusOperation = false;
-            isMinusOperation = false;
-            isDivideOperation = false;
-        } else if (view.getId() == R.id.btn_divide) {
-            first = Double.parseDouble(result.getText().toString());
-            isDivideOperation = true;
-            isPlusOperation = false;
-            isMinusOperation = false;
-            isMultiplyOperation = false;
+            //нажимаем на умножение
+            first = Integer.parseInt(result.getText().toString());
         } else if (view.getId() == R.id.btn_equal) {
-            second = Double.parseDouble(result.getText().toString());
+            //нажимаем на равно
+            second = Integer.parseInt(result.getText().toString());
 
-            if (isPlusOperation) {
-                setResult(first + second);
-            } else if (isMinusOperation) {
-                setResult(first - second);
-            } else if (isMultiplyOperation) {
-                setResult(first * second);
-            } else if (isDivideOperation) {
-                if (second != 0) {
-                    setResult(first / second);
-                } else {
-                    result.setText("ошибка");
-                }
-            }
 
-            isPlusOperation = false;
-            isMinusOperation = false;
-            isMultiplyOperation = false;
-            isDivideOperation = false;
-            btn_equal.setEnabled(false);
+            int minus = first - second;
+            int plus = first + second;
+            int divide = first * second;
+            int multiplication = first / second;
+            result.setText(String.valueOf(minus));
+            result.setText(String.valueOf(plus));
+            result.setText(String.valueOf(divide));
+            result.setText(String.valueOf(multiplication));
         }
-
         isOperationClick = true;
-    }
 
-
-    private void setResult(double value) {
-        if (isInteger(value)) {
-            result.setText(String.valueOf((int) value));
-        } else {
-            result.setText(String.valueOf(value));
-        }
-    }
-
-    private boolean isInteger(double value) {
-        return value == (int) value;
     }
 
     public void onDotClick(View view) {
-        String currentText = result.getText().toString();
-        if (!currentText.contains(".")) {
-            result.append(".");
-        }
     }
 }
+
+
