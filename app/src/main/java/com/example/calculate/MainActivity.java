e0c59c770dd5f8de23492055bc73a2605a3adfa3
kairatabdillaev <kajratabdillaev@gmail.com>
2025-02-19 17:34:09 +0600

    never give up

--- app/src/main/java/com/example/calculate/MainActivity.java
+++ app/src/main/java/com/example/calculate/MainActivity.java
@@ -1,73 +1,126 @@
 package com.example.calculate;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
 
 import com.google.android.material.button.MaterialButton;
 
 public class MainActivity extends AppCompatActivity {
-    private TextView result;
-    private boolean isOperationClick;
-    private int first, second;
+    private TextView textView;
+    private int firstOperand, secondOperand;
+    private boolean is0perationClich;
+    String operator = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        // EdgeToEdge.enable(this);
+
         setContentView(R.layout.activity_main);
 
-        result = findViewById(R.id.result);
+        // ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+        //     Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+        //   v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+        //    return insets;
+        //  });
+        textView = findViewById(R.id.result);
+        Log.d("lolo","onCreate");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d("lolo","onStart");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d("lolo","onResume");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d("lolo","onPause");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d("lolo","onPause");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d("lolo","onStop");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();Log.d("lolo","onDestroy");
     }
 
     public void oneClick(View view) {
+
         String textButton = ((MaterialButton) view).getText().toString();
+
         if (textButton.equals("AC")) {
-            result.setText("0");
-        } else if (result.getText().toString().equals("0") || isOperationClick) {
-            result.setText(textButton);
+            textView.setText("0");
+            firstOperand = 0;
+
+        } else if (textView.getText().toString().equals("0") || is0perationClich) {
+            textView.setText(textButton);
         } else {
-            result.append(textButton);
+            textView.append(textButton);
         }
-        isOperationClick = false;
+
+        is0perationClich = false;
     }
 
     public void oneOperationClick(View view) {
-        if (view.getId() == R.id.btn_minus) {
-            //нажимаем на минус
-            first = Integer.parseInt(result.getText().toString());
+        Button button = (Button) view;
+        if (view.getId() != R.id.btn_equal) {
+            firstOperand = Integer.parseInt(textView.getText().toString());
+            operator = button.getText().toString();
+        } else {
+            secondOperand = Integer.parseInt(textView.getText().toString());
+            // 33         12             21
+            int result;
 
-        } else if (view.getId() == R.id.btn_plus) {
-            //нажимаем на плюс
-            first = Integer.parseInt(result.getText().toString());
-        }else if (view.getId() == R.id.btn_divide) {
-            //нажимаем на деление
-            first = Integer.parseInt(result.getText().toString());
-        } else if (view.getId() == R.id.btn_multiply) {
-            //нажимаем на умножение
-            first = Integer.parseInt(result.getText().toString());
-        } else if (view.getId() == R.id.btn_equal) {
-            //нажимаем на равно
-            second = Integer.parseInt(result.getText().toString());
+            switch (operator) {
+                case "/":
+                    result = firstOperand / secondOperand;
+                    break;
+                case "X":
+                    result = firstOperand * secondOperand;
+                    break;
+                case "+":
+                    result = firstOperand + secondOperand;
+                    break;
+                default:
+                    result = firstOperand - secondOperand;
+            }
+            textView.setText(String.valueOf(result));
 
-
-            int minus = first - second;
-            int plus = first + second;
-            int divide = first * second;
-            int multiplication = first / second;
-            result.setText(String.valueOf(minus));
-            result.setText(String.valueOf(plus));
-            result.setText(String.valueOf(divide));
-            result.setText(String.valueOf(multiplication));
         }
-        isOperationClick = true;
 
-    }
-
-    public void onDotClick(View view) {
+        is0perationClich = true;
     }
 }
+
+
 
 
