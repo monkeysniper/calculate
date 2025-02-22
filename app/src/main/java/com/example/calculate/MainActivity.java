6bcc1711912b6cc7e77104c99a7e8a8d41903395
kairatabdillaev <kajratabdillaev@gmail.com>
2025-02-22 17:38:24 +0600

    never give up

--- app/src/main/java/com/example/calculate/MainActivity.java
+++ app/src/main/java/com/example/calculate/MainActivity.java
@@ -1,126 +1,89 @@
 package com.example.calculate;
 
-import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 import com.google.android.material.button.MaterialButton;
 
 public class MainActivity extends AppCompatActivity {
     private TextView textView;
+    private Button btnResultActivity;
     private int firstOperand, secondOperand;
-    private boolean is0perationClich;
-    String operator = "";
+    private boolean isOperationClick;
+    private String operator;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        // EdgeToEdge.enable(this);
-
         setContentView(R.layout.activity_main);
 
-        // ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-        //     Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-        //   v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-        //    return insets;
-        //  });
         textView = findViewById(R.id.result);
-        Log.d("lolo","onCreate");
-    }
+        btnResultActivity = findViewById(R.id.btn_result);
 
-    @Override
-    protected void onStart() {
-        super.onStart();
-        Log.d("lolo","onStart");
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        Log.d("lolo","onResume");
-    }
-
-    @Override
-    protected void onRestart() {
-        super.onRestart();
-        Log.d("lolo","onPause");
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-        Log.d("lolo","onPause");
-    }
-
-    @Override
-    protected void onStop() {
-        super.onStop();
-        Log.d("lolo","onStop");
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();Log.d("lolo","onDestroy");
+        btnResultActivity.setOnClickListener(v -> {
+            Intent intent = new Intent(MainActivity.this, MainActivity2.class);
+            intent.putExtra("result_key", textView.getText().toString());
+            startActivity(intent);
+        });
     }
 
     public void oneClick(View view) {
+        if (btnResultActivity.getVisibility() == View.VISIBLE) {
+            btnResultActivity.setVisibility(View.GONE);
+        }
 
         String textButton = ((MaterialButton) view).getText().toString();
 
         if (textButton.equals("AC")) {
             textView.setText("0");
             firstOperand = 0;
-
-        } else if (textView.getText().toString().equals("0") || is0perationClich) {
+        } else if (textView.getText().toString().equals("0") || isOperationClick) {
             textView.setText(textButton);
         } else {
             textView.append(textButton);
         }
-
-        is0perationClich = false;
+        isOperationClick = false;
     }
 
     public void oneOperationClick(View view) {
-        Button button = (Button) view;
-        if (view.getId() != R.id.btn_equal) {
-            firstOperand = Integer.parseInt(textView.getText().toString());
-            operator = button.getText().toString();
-        } else {
-            secondOperand = Integer.parseInt(textView.getText().toString());
-            // 33         12             21
-            int result;
-
-            switch (operator) {
-                case "/":
-                    result = firstOperand / secondOperand;
-                    break;
-                case "X":
-                    result = firstOperand * secondOperand;
-                    break;
-                case "+":
-                    result = firstOperand + secondOperand;
-                    break;
-                default:
-                    result = firstOperand - secondOperand;
-            }
-            textView.setText(String.valueOf(result));
-
+        if (view.getId() != R.id.btn_equal && btnResultActivity.getVisibility() == View.VISIBLE) {
+            btnResultActivity.setVisibility(View.GONE);
         }
 
-        is0perationClich = true;
+        if (view.getId() == R.id.btn_plus) {
+            firstOperand = Integer.parseInt(textView.getText().toString());
+            operator = "+";
+        } else if (view.getId() == R.id.btn_minus) {
+            firstOperand = Integer.parseInt(textView.getText().toString());
+            operator = "-";
+        } else if (view.getId() == R.id.btn_multiply) {
+            firstOperand = Integer.parseInt(textView.getText().toString());
+            operator = "*";
+        } else if (view.getId() == R.id.btn_divide) {
+            firstOperand = Integer.parseInt(textView.getText().toString());
+            operator = "/";
+        } else if (view.getId() == R.id.btn_equal) {
+            secondOperand = Integer.parseInt(textView.getText().toString());
+            int result = 0;
+
+            if (operator.equals("+")) {
+                result = firstOperand + secondOperand;
+            } else if (operator.equals("-")) {
+                result = firstOperand - secondOperand;
+            } else if (operator.equals("*")) {
+                result = firstOperand * secondOperand;
+            } else if (operator.equals("/")) {
+                result = firstOperand / secondOperand;
+            }
+            textView.setText(String.valueOf(result));
+            btnResultActivity.setVisibility(View.VISIBLE);
+        }
+        isOperationClick = true;
     }
+
 }
-
-
-
-
--- app/src/main/java/com/example/calculate/MainActivity2.java
+++ app/src/main/java/com/example/calculate/MainActivity2.java
@@ -1,6 +1,10 @@
 package com.example.calculate;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,16 +13,35 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity2 extends AppCompatActivity {
+    private TextView textView;
 
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main2);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        TextView resultOperation=findViewById(R.id.resultOperation);
+        String result = getIntent().getStringExtra("result_key");
+        resultOperation.setText(result );
+
+        Button btnNext = findViewById(R.id.next);
+        ImageView imgHeart = findViewById(R.id.heart);
+        imgHeart.setOnClickListener(v -> {
+            Boolean isLiked = (Boolean) imgHeart.getTag();
+            if(isLiked == null) {
+                isLiked = false;
+            }
+            if(!isLiked) {
+                imgHeart.setImageResource(R.drawable.goida1);
+                imgHeart.setTag(true);
+            } else {
+                imgHeart.setImageResource(R.drawable.ukrai);
+                imgHeart.setTag(false);
+            }
+        });
+        btnNext.setOnClickListener(v -> {
+            finishAffinity();
         });
     }
 }
